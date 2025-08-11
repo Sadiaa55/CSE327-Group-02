e848cc48222a19d58b69000c88075e01c0a475be
Sadia Islam Mou <sadiaislammou55@gmail.com>
2025-08-11 22:06:12 +0600

    feat: Sadia's part-core folder

---
+++ flutter_application_1/lib/core/route/app_route_name.dart
@@ -0,0 +1,7 @@
+class AppRouteName {
+  static const String splash = "/splash";
+  static const String home = "/";
+  static const String movieDetail = "/movie-detail";
+  static const String movieBooking = "/movie-booking";
+  static const String wishlist = '/wishlist';
+}
